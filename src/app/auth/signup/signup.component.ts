449c5f91418f74c2396071301f247435575bf354
Etno Polino <einshoch66@gmail.com>
2024-02-20 18:51:35 +0100

    set up signup html page

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css'],
+})
+export class SignupComponent implements OnInit {
+  signupForm!: FormGroup; // la property called signupForm with type FormGroup | undefined. It starts with a value of undefined.
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+    });
+  }
+}
