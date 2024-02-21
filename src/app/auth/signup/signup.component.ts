112da6edbf8fa4552b754be60020264bebc26bd7
Etno Polino <einshoch66@gmail.com>
2024-02-21 17:18:50 +0100

    Implement user registration
    sending mail not working but user get written in the database

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { SignupRequestPayload } from './Signup-request.payload';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,13 +9,33 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  signupRequestPayload!: SignupRequestPayload;
+
   signupForm!: FormGroup; // la property called signupForm with type FormGroup | undefined. It starts with a value of undefined.
+
+  constructor(private authService: AuthService) {
+    this.signupRequestPayload = {
+      username: '',
+      email: '',
+      password: '',
+    };
+  }
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       username: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', Validators.required),
+    });
+  }
+
+  signup() {
+    this.signupRequestPayload.username = this.signupForm.get('username')?.value;
+    this.signupRequestPayload.email = this.signupForm.get('email')?.value;
+    this.signupRequestPayload.password = this.signupForm.get('password')?.value;
+
+    this.authService.signup(this.signupRequestPayload).subscribe((data) => {
+      console.log(data);
     });
   }
 }
