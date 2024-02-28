acd11ccd285709ff361931ea55154125dc1f851b
Etno Polino <einshoch66@gmail.com>
2024-02-28 19:22:32 +0100

    refacto home âge component (sidebar + subreddit bar + post title component)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { TokenInterceptorService } from './token-interceptor.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { PostTileComponent } from './shared/post-tile/post-tile.component';
+import { SideBarComponent } from './shared/side-bar/side-bar.component';
+import { SubredditSideBarComponent } from './shared/subreddit-side-bar/subreddit-side-bar.component';
+import { VoteButtonComponent } from './shared/vote-button/vote-button.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +26,10 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     SignupComponent,
     LoginComponent,
     HomeComponent,
+    PostTileComponent,
+    SideBarComponent,
+    SubredditSideBarComponent,
+    VoteButtonComponent,
   ],
   imports: [
     BrowserModule,
