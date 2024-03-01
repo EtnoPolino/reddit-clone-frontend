7609813e8fa2207e4df31187a665e27c04acc56d
Etno Polino <einshoch66@gmail.com>
2024-03-01 19:36:02 +0100

    Create subreddits and Posts
    utilisation et installation de l'API Tinymce

--- src/app/shared/post.service.ts
+++ src/app/shared/post.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { PostModel } from './post-model';
+import { CreatePostPayload } from '../post/create-post/create-post.payload';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +13,13 @@ export class PostService {
   getAllPost(): Observable<Array<PostModel>> {
     return this.httpClient.get<Array<PostModel>>(
       'http://localhost:8080/api/posts'
+    );
+  }
+
+  createPost(postPayload: CreatePostPayload): Observable<CreatePostPayload> {
+    return this.httpClient.post<CreatePostPayload>(
+      'http://localhost:8080/api/posts',
+      postPayload
     );
   }
 }
--- src/app/shared/side-bar/side-bar.component.ts
+++ src/app/shared/side-bar/side-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-bar.component.css'],
 })
 export class SideBarComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
   ngOnInit(): void {}
+
+  goToCreateSubreddit() {
+    this.router.navigateByUrl('/create-subreddit');
+  }
+  goToCreatePost() {
+    this.router.navigateByUrl('/create-post');
+  }
 }
